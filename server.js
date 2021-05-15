4b43fdbef84e1138d797a620dd7846bba1f06859
madtv129 <madtv129@gmail.com>
2021-05-15 02:58:05 -0700

    figuring out how to connect back end to front end

--- server.js
+++ server.js
@@ -7,10 +7,9 @@ const fs = require("fs")
 
 //pathings
 app.use('/css', express.static('assets/css'));
-app.use('/img', express.static('assets/img'));
-
+app.use('/img', express.static('assets/imgs'));
 app.set('view-engine', 'hbs')
-console.log(__dirname);
+
 //TEST DATABASE
 const db = mysql.createConnection({
     host: process.env.databaseHost,
@@ -19,6 +18,36 @@ const db = mysql.createConnection({
     database: process.env.database
 })
 //http://localhost/phpmyadmin/
+
+app.use(session( {
+        secret:'A random secret',
+        name:'YourSessionID',
+        resave: false,
+        saveUninitialized: true 
+    }));
+
+async function initializeDB() {
+
+    const mysql = require('mysql2/promise');
+
+    const createDBAndTables = `CREATE DATABASE IF NOT EXISTS test;
+        use test;
+        CREATE TABLE IF NOT EXISTS user (
+        ID int NOT NULL AUTO_INCREMENT,
+        email varchar(30),
+        password varchar(30),
+        PRIMARY KEY (ID));`;
+
+    await db.query(createDBAndTables);
+    let results = await debugPort.query("SELECT COUNT(*) FROM user");
+    let count = results[0][0]['COUNT(*)'];
+
+    if(count < 1) {
+        results = await db.query("INSERT INTO user (email, password) values ('arron_ferguson@bcit.ca', 'admin')");
+        console.log("Added one user record.");
+    }
+    db.end();
+}
 
 db.connect( (error) => {
     if(error) {
@@ -33,42 +62,46 @@ app.use(express.json())
 
 app.get('/', (req, res) => {
     res.render('index.hbs')
+    initalizeDB()
 })
 
-app.get('/login', (req, res) => {
-    res.render('login.html')
+app.get('/loggedin', (req, res) => {
+    res.render('loggedin.hbs')
 })
 
-app.get('/users', (req, res) => {
-    res.json(users)
-})
-
-app.post('/users/', (req, res) => {
+app.post('/authen', (req, res) => {
     try {
-    const user = { name: req.body.name, password: req.body.password }
+    const user = { email: req.body.email, password: req.body.password }
     user.push(user)
     res.status(201).send()
     } catch {
-        res.status(500).send()
+        res.status(404).send()
     }
 })
 
-app.post('/users/login', async (req, res) => {
-    const user = users.find(user => user.name = req.body.name)
-    if (user == null) {
-        return res.status(400).send('Cannot find user')
-    }
-    try {
-        if(await req.body.password == user.password) {
-            res.send('You\'ve successfully logged in')
-        }
-        else {
-            res.send('Username or Password was incorrect.')
-        }
-    } catch {
-        res.status(500).send()
-    }
-})
+function auth(email, pwd) {
+    const {email, password} = req.body
 
-/
+    if(!email || !password) {
+        return res.status(400).render('login', { 
+            message: 'Please provide email and password'
+        })
+    }
+
+    db.query('SELECT * FROM user WHERE email = ? AND password = ?', [email, password], 
+    async (error, results) => {
+        if (error) {
+            console.log(error)
+        }
+        
+        if(!results || !password) {
+            res.status(401).render('login', {
+                message: 'Email or Password was incorrect'
+            })
+        } else {
+            const id = results[0].id
+            
+        }
+    })
+}
 app.listen(3000)
